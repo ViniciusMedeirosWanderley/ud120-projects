8c32526c5ce1fd08eb3f834dd19d40d8d0b35cfc
Vinícius Medeiros Wanderley <viniciusmedeiros2571@gmail.com>
2019-03-13 09:49:01 -0300

    Choose Your Own

--- choose_your_own/your_algorithm.py
+++ choose_your_own/your_algorithm.py
@@ -1,26 +1,33 @@
 #!/usr/bin/python
 
 import matplotlib.pyplot as plt
+from time import time
 from prep_terrain_data import makeTerrainData
 from class_vis import prettyPicture
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
 
 features_train, labels_train, features_test, labels_test = makeTerrainData()
 
 
-### the training data (features_train, labels_train) have both "fast" and "slow"
-### points mixed together--separate them so we can give them different colors
-### in the scatterplot and identify them visually
-grade_fast = [features_train[ii][0] for ii in range(0, len(features_train)) if labels_train[ii]==0]
-bumpy_fast = [features_train[ii][1] for ii in range(0, len(features_train)) if labels_train[ii]==0]
-grade_slow = [features_train[ii][0] for ii in range(0, len(features_train)) if labels_train[ii]==1]
-bumpy_slow = [features_train[ii][1] for ii in range(0, len(features_train)) if labels_train[ii]==1]
+# the training data (features_train, labels_train) have both "fast" and "slow"
+# points mixed together--separate them so we can give them different colors
+# in the scatterplot and identify them visually
+grade_fast = [features_train[ii][0]
+              for ii in range(0, len(features_train)) if labels_train[ii] == 0]
+bumpy_fast = [features_train[ii][1]
+              for ii in range(0, len(features_train)) if labels_train[ii] == 0]
+grade_slow = [features_train[ii][0]
+              for ii in range(0, len(features_train)) if labels_train[ii] == 1]
+bumpy_slow = [features_train[ii][1]
+              for ii in range(0, len(features_train)) if labels_train[ii] == 1]
 
 
-#### initial visualization
+# initial visualization
 plt.xlim(0.0, 1.0)
 plt.ylim(0.0, 1.0)
-plt.scatter(bumpy_fast, grade_fast, color = "b", label="fast")
-plt.scatter(grade_slow, bumpy_slow, color = "r", label="slow")
+plt.scatter(bumpy_fast, grade_fast, color="b", label="fast")
+plt.scatter(grade_slow, bumpy_slow, color="r", label="slow")
 plt.legend()
 plt.xlabel("bumpiness")
 plt.ylabel("grade")
@@ -28,17 +35,26 @@ plt.show()
 ################################################################################
 
 
-### your code here!  name your classifier object clf if you want the 
-### visualization code (prettyPicture) to show you the decision boundary
+# your code here!  name your classifier object clf if you want the
+# visualization code (prettyPicture) to show you the decision boundary
 
+clf = RandomForestClassifier(n_estimators=20,
+                             criterion="entropy",
+                             min_samples_split=50,
+                             verbose=0)
+# train
+t0 = time()
+clf = clf.fit(features_train, labels_train)
+print("Training Time: {0:.3f}".format(time()-t0))
 
+# test
+t1 = time()
+pred = clf.predict(features_test)
+print("Test Time: {0:.3f}".format(time()-t1))
 
+# evaluate
+acc = accuracy_score(labels_test, pred)
+print(acc)
 
-
-
-
-
-try:
-    prettyPicture(clf, features_test, labels_test)
-except NameError:
-    pass
+# visualize
+prettyPicture(clf, features_test, labels_test)
