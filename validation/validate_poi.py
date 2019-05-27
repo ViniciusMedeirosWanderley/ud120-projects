b9adea49d1f0de5b5d0ecf27bf93215713b8052d
Vinícius M. Wanderley <viniciusmedeiros2571@gmail.com>
2019-05-27 19:04:26 -0300

    Validation

--- validation/validate_poi.py
+++ validation/validate_poi.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python
 
 
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.tree import DecisionTreeClassifier
 """
     Starter code for the validation mini-project.
     The first step toward building your POI identifier!
@@ -11,22 +14,28 @@
 """
 
 import pickle
-import sys
-sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
 
-data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "r") )
+data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "rb"))
 
-### first element is our labels, any added elements are predictor
-### features. Keep this the same for the mini-project, but you'll
-### have a different feature list when you do the final project.
+# first element is our labels, any added elements are predictor
+# features. Keep this the same for the mini-project, but you'll
+# have a different feature list when you do the final project.
 features_list = ["poi", "salary"]
 
 data = featureFormat(data_dict, features_list)
 labels, features = targetFeatureSplit(data)
 
 
+# it's all yours from here forward!
 
-### it's all yours from here forward!  
+clf = DecisionTreeClassifier()
+clf.fit(features, labels)
+pred = clf.predict(features)
+print(accuracy_score(pred, labels))
 
-
+features_train, features_test, labels_train, labels_test = train_test_split(
+    features, labels, test_size=0.3, random_state=42)
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
+print(accuracy_score(pred, labels_test))
