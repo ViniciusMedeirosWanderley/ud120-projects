5cfa2a4f85bfe7adb220067e26633fc9f262453f
Vinícius M. Wanderley <viniciusmedeiros2571@gmail.com>
2019-05-28 12:42:25 -0300

    Evaluation

--- evaluation/evaluate_poi_identifier.py
+++ evaluation/evaluate_poi_identifier.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python
 
 
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, precision_score, recall_score, confusion_matrix
+from sklearn.tree import DecisionTreeClassifier
 """
     Starter code for the evaluation mini-project.
     Start by copying your trained/tested POI identifier from
@@ -13,19 +16,26 @@
 
 import pickle
 import sys
-sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
 
-data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "r") )
+data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "rb"))
 
-### add more features to features_list!
+# add more features to features_list!
 features_list = ["poi", "salary"]
 
 data = featureFormat(data_dict, features_list)
 labels, features = targetFeatureSplit(data)
 
 
+# your code goes here
 
-### your code goes here 
 
-
+features_train, features_test, labels_train, labels_test = train_test_split(
+    features, labels, test_size=0.3, random_state=42)
+clf = DecisionTreeClassifier()
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
+print('Acc:', accuracy_score(labels_test, pred))
+print('Confusion Matrix:', confusion_matrix(labels_test, pred))
+print('Precision Score:', precision_score(labels_test, pred))
+print('Recal Score:', recall_score(labels_test, pred))
